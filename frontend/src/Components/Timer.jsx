978ff221733ab0e71c2f9d9e40e9142ba9256076
roba <10408936+roba-adnew@users.noreply.github.com>
2024-09-19 17:10:01 -0400

    updated tagging area for duck in intersection.jpg

--- frontend/src/Components/Timer.jsx
+++ frontend/src/Components/Timer.jsx
@@ -6,7 +6,7 @@ function formattedTime(timeMS) {
     const totalSeconds = timeMS / 1000; // time in API in MS;
 
     const minutes = Math.floor(totalSeconds / 60);
-    const displayMinutes = `${minutes.toString().padStart(2, "0")}`;
+    const displayMinutes = `${minutes.toString().padStart(1, "0")}`;
 
     const seconds = Math.floor(totalSeconds % 60);
     const displaySeconds = `${seconds.toString().padStart(2, "0")}`;
