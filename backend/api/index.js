06e1b9da6fbac684e30069c2dae31ed73617688f
roba <10408936+roba-adnew@users.noreply.github.com>
2024-10-26 15:16:27 -0400

    adding in middleware to set headers specifically for vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -48,6 +48,20 @@ app.use(
 );
 
 app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.use((req, res, next) => {
     debug('Session ID:', req.sessionID);
     debug('Session:', req.session);
     next();
